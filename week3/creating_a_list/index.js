7148b5e06a53e9b272e279e48b59ce6eaaf0ba94
LJohnson-Bryan <logan.johnson@bryanuniversity.edu>
2020-08-14 12:23:57 -0400

    Creating A List

---
+++ week3/creating_a_list/index.js
@@ -0,0 +1,25 @@
+/*
+Step 2: Commented out for step 3 with repeated Hello World.
+
+var newHeading = document.createElement("h1");
+newHeading.textContent = "Hello World";
+document.body.append(newHeading);
+*/
+
+// Steps 3 + 4 -- running for loop to show "Hello World" 10 times. Adding style for step #4.
+for(var i = 0; i < 10; i++) {
+    var newHeading = document.createElement("h1");
+    newHeading.textContent = "Hello World";
+    newHeading.style.backgroundColor = "red";
+    newHeading.style.color = "white";
+    document.body.append(newHeading);
+}
+
+
+const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
+
+for(var i = 0; i < names.length; i++) {
+    var newParagraph = document.createElement("p");
+    newParagraph.textContent = names[i];
+    document.body.append(newParagraph);
+}
